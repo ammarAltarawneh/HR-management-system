421906642a41c394189ffddd26310a759062d6c5
ammarAltarawneh <tarawnhammar@gmail.com>
2023-04-11 00:11:13 +0000

    updated

--- J S/app.js
+++ J S/app.js
@@ -1,34 +1,36 @@
 'use strict';
-function Employee (employeeId, fullName, department, level){
-    this.employeeId = employeeId;
-    this.fullName = fullName;
-    this.department = department;
-    this.level = level;
-    this.imageURL = `./assets/${this.fullName}.jpg`;
-    this.salary = this.calculateSalary();
+function Employee (Id, fullName, department, level){
+  this.Id = Id;
+  this.fullName = fullName;
+  this.department = department;
+  this.level = level;
+  this.imageURL = `./assets/${this.fullName}.jpg`;
+  this.salary = this.calculateSalary();
 }
 
 Employee.prototype.calculateSalary = function() {
-    let minSalary, maxSalary;
-    if (this.level === "Senior") {
-        minSalary = 1500;
-        maxSalary = 2000;
-      } else if (this.level === "Mid-Senior") {
-        minSalary = 1000;
-        maxSalary = 1500;
-      } else if (this.level === "Junior") {
-        minSalary = 500;
-        maxSalary = 1000;
-      }
-    const randomSalary = Math.floor(Math.random() * (maxSalary - minSalary + 1)) + minSalary;
-    const netSalary = randomSalary - randomSalary * 0.075;
-    return netSalary;
-  };
-  
+  let minSalary, maxSalary;
+  if (this.level === "Senior") {
+      minSalary = 1500;
+      maxSalary = 2000;
+    } else if (this.level === "Mid-Senior") {
+      minSalary = 1000;
+      maxSalary = 1500;
+    } else if (this.level === "Junior") {
+      minSalary = 500;
+      maxSalary = 1000;
+    }
+  const randomSalary = (Math.random() * (maxSalary - minSalary)) + minSalary;
+  const netSalary = Math.floor(randomSalary - randomSalary * 0.075);
+  return netSalary;
+};
 
-  Employee.prototype.render = function() {
-    document.write(`<h1>${this.fullName}</h1>
-                    <p>Salary: ${this.calculateSalary()}</p>`);
+
+Employee.prototype.render = function() {
+  document.write(`<h1>Id: ${this.Id}</h1>
+                  <p>Full name: ${this.fullName}</p>
+                  <p>Department: ${this.department}</p>
+                  <p>Salary: ${this.calculateSalary()}</p>`);
 };
 
 
