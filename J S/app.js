433ce3f9dd8a24bfefd4ef83a829c10977667506
ammarAltarawneh <tarawnhammar@gmail.com>
2023-04-19 04:30:50 +0000

    updated

--- J S/app.js
+++ J S/app.js
@@ -1,35 +1,44 @@
-'use strict';
+'use strict'
+
 
 let allEmployees = [];
-let form = document.getElementById('HR-manag');
-form.addEventListener('submit', addNewEmployee)
+let formEl = document.getElementById('employee-Form');
+formEl.addEventListener("submit", submitHand);
 
-function Employee(fullName, department, level, imgUrl) {
+function Employee(employeeId, fullName, department, level, imageURL, salary) {
+
+  this.employeeId = employeeId;
   this.fullName = fullName;
   this.department = department;
+  this.imageURL = imageURL;
   this.level = level;
-  this.imgUrl = imgUrl;
-  allEmployees.push(this)
+  this.salary = salary;
+  allEmployees.push(this);
+
 }
 
-Employee.prototype.calculateSalary = function () {
-  let minSalary, maxSalary;
-  if (this.level === "Senior") {
-    minSalary = 1500;
-    maxSalary = 2000;
-  } else if (this.level === "Mid-Senior") {
-    minSalary = 1000;
-    maxSalary = 1500;
-  } else if (this.level === "Junior") {
-    minSalary = 500;
-    maxSalary = 1000;
-  }
-  const randomSalary = (Math.random() * (maxSalary - minSalary)) + minSalary;
-  const netSalary = Math.floor(randomSalary - randomSalary * 0.075);
-  return netSalary;
-};
+function calculateSalary() {
+  for (let i = 0; i < allEmployees.length; i++) {
 
-Employee.prototype.generateId = function () {
+    if (allEmployees[i].level == "Senior") {
+      allEmployees[i].salary = Math.round(Math.floor(Math.random() * (2000 - 1500) + 1500))
+      allEmployees[i].salary = parseInt(allEmployees[i].salary - (allEmployees[i].salary * 7.5 / 100))
+      return allEmployees[i].salary;
+    }
+    else if (allEmployees[i].level == "Mid-senior") {
+      allEmployees[i].salary = Math.round(Math.floor(Math.random() * (1500 - 1000) + 1000))
+      allEmployees[i].salary = parseInt(allEmployees[i].salary - (allEmployees[i].salary * 7.5 / 100))
+      return allEmployees[i].salary;
+    }
+    else {
+      allEmployees[i].salary = Math.round(Math.floor(Math.random() * (1000 - 500) + 500))
+      allEmployees[i].salary = parseInt(allEmployees[i].salary - (allEmployees[i].salary * 7.5 / 100))
+      return allEmployees[i].salary;
+    }
+  }
+}
+
+function generateId() {
   let employeeId = '';
   for (let i = 0; i < 4; i++) {
     employeeId += Math.floor(Math.random() * 10);
@@ -37,52 +46,181 @@ Employee.prototype.generateId = function () {
   return employeeId;
 }
 
-
-Employee.prototype.render = function () {
-  const section = document.getElementById('container');
-
-  const divEl = document.createElement('div');
-  section.appendChild(divEl);
-  divEl.setAttribute('style', 'display: flex; flex-direction: column; align-items: center; background-color: #0E8388; width: 30%; border-radius: 10px; padding-block: 20px ;margin: 0 auto; margin-bottom: 20px')
-
-  let imgEl = document.createElement('img');
-  divEl.appendChild(imgEl);
-  imgEl.src = `./assets/${this.fullName}.jpg`;
-  imgEl.alt = "Employee pic";
-  imgEl.setAttribute('style', 'width: 30%; height: auto;  border-radius: 50%;')
-
-  let h2El2 = document.createElement('h2');
-  h2El2.textContent = `Name: ${this.fullName} - ID: ${this.generateId()}`;
-  divEl.appendChild(h2El2);
-  h2El2.setAttribute('style', 'color:white')
-
-  let h3El3 = document.createElement('h3');
-  h3El3.textContent = `Department: ${this.department} - Level: ${this.level}`;
-  divEl.appendChild(h3El3);
-  h3El3.setAttribute('style', 'color:white')
-
-  let h4El4 = document.createElement('h4');
-  h4El4.textContent = `Salary: ${this.calculateSalary()}`;
-  divEl.appendChild(h4El4);
-  h4El4.setAttribute('style', 'color:white')
+function addEmployeeJson() {
+  let jsonArray = localStorage.getItem('allEmployees')
+  let getInformationEmployee = JSON.parse(jsonArray);
+  allEmployees = getInformationEmployee;
 
 }
 
-function addNewEmployee(event) {
-  event.preventDefault();
-  let fullName = event.target.name.value;
-  let department = event.target.dropdown1.value;
-  let level = event.target.dropdown2.value;
-  let imgUrl = event.target.url.value;
+function submitHand(event) {
+  let fullname = event.target.fname.value;
+  let Department = event.target.Department.value;
+  let levels = event.target.level.value;
+  let image = event.target.imgurl.value
+  let id = generateId();
+  let salary = calculateSalary();
+  let newEmployee = new Employee(id, fullname, Department, levels, image, salary)
+  let jsonArryEmployee = JSON.stringify(allEmployees);
+  localStorage.setItem('allEmployees', jsonArryEmployee);
 
-  let newEmployee = new Employee(fullName, department, level, imgUrl);
-
-  let jsonArr = JSON.stringify(allEmployees);
-  localStorage.setItem('allEmployees',jsonArr)
-
-  newEmployee.calculateSalary();
-  newEmployee.generateId();
-  newEmployee.render();
 }
+
+function rander() {
+  addEmployeeJson();
+  if (allEmployees == null) {
+    allEmployees = [];
+  }
+
+  for (let i = 0; i < allEmployees.length; i++) {
+    if (allEmployees[i].department == 'Administration') {
+
+      const section = document.getElementById("Administration");
+
+      let divElback = document.createElement('div')
+      section.appendChild(divElback);
+      divElback.setAttribute('style', 'display: flex; flex-direction: column; background-color: #0E8388; width: 100%; border-radius: 10px; padding-block: 20px; margin-bottom: 20px')
+
+
+      let imgEl = document.createElement('img')
+      divElback.appendChild(imgEl);
+      imgEl.src = `./assets/${allEmployees[i].fullName}.jpg`;
+      imgEl.setAttribute('style', 'width: 40%; height: auto;  border-radius: 50%;margin-left: 30%;')
+
+      let divElInfo = document.createElement('div');
+      divElback.appendChild(divElInfo);
+      divElInfo.setAttribute('style', 'display: flex; flex-direction: column; align-items: center; background-color: white; width: 90%; border-radius: 10px; padding-block: 20px ;margin:0 auto; margin-top: 20px;hight:100px')
+
+
+      let h2El2 = document.createElement('h2');
+      h2El2.textContent = `Name: ${allEmployees[i].fullName} - ID: ${allEmployees[i].employeeId}`;
+      divElInfo.appendChild(h2El2);
+      h2El2.setAttribute('style', 'color:blue')
+
+      let h3El3 = document.createElement('h3');
+      h3El3.textContent = `Department: ${allEmployees[i].department} - Level: ${allEmployees[i].level}`;
+      divElInfo.appendChild(h3El3);
+      h3El3.setAttribute('style', 'color:blue;')
+
+      let h4El4 = document.createElement('h4');
+      h4El4.textContent = `Salary: ${allEmployees[i].salary}`;
+      divElInfo.appendChild(h4El4);
+      h4El4.setAttribute('style', 'color:blue')
+
+    } else if (allEmployees[i].department == 'Marketing') {
+
+      const section = document.getElementById("Marketing");
+
+      let divElback = document.createElement('div')
+      section.appendChild(divElback);
+      divElback.setAttribute('style', 'display: flex; flex-direction: column; background-color: #0E8388; width: 100%; border-radius: 10px; padding-block: 20px; margin-bottom: 20px')
+
+
+      let imgEl = document.createElement('img')
+      divElback.appendChild(imgEl);
+      imgEl.src = `./assets/${allEmployees[i].fullName}.jpg`;
+      imgEl.setAttribute('style', 'width: 40%; height: auto;  border-radius: 50%;margin-left: 30%;')
+
+      let divElInfo = document.createElement('div');
+      divElback.appendChild(divElInfo);
+      divElInfo.setAttribute('style', 'display: flex; flex-direction: column; align-items: center; background-color: white; width: 90%; border-radius: 10px; padding-block: 20px ;margin:0 auto; margin-top: 20px;hight:100px')
+
+
+      let h2El2 = document.createElement('h2');
+      h2El2.textContent = `Name: ${allEmployees[i].fullName} - ID: ${allEmployees[i].employeeId}`;
+      divElInfo.appendChild(h2El2);
+      h2El2.setAttribute('style', 'color:blue')
+
+      let h3El3 = document.createElement('h3');
+      h3El3.textContent = `Department: ${allEmployees[i].department} - Level: ${allEmployees[i].level}`;
+      divElInfo.appendChild(h3El3);
+      h3El3.setAttribute('style', 'color:blue;')
+
+      let h4El4 = document.createElement('h4');
+      h4El4.textContent = `Salary: ${allEmployees[i].salary}`;
+      divElInfo.appendChild(h4El4);
+      h4El4.setAttribute('style', 'color:blue')
+
+    } else if (allEmployees[i].department == 'Development') {
+
+      const section = document.getElementById("Development");
+
+      let divElback = document.createElement('div')
+      section.appendChild(divElback);
+      divElback.setAttribute('style', 'display: flex; flex-direction: column; background-color: #0E8388; width: 100%; border-radius: 10px; padding-block: 20px; margin-bottom: 20px')
+
+
+      let imgEl = document.createElement('img')
+      divElback.appendChild(imgEl);
+      imgEl.src = `./assets/${allEmployees[i].fullName}.jpg`;
+      imgEl.setAttribute('style', 'width: 40%; height: auto;  border-radius: 50%;margin-left: 30%;')
+
+      let divElInfo = document.createElement('div');
+      divElback.appendChild(divElInfo);
+      divElInfo.setAttribute('style', 'display: flex; flex-direction: column; align-items: center; background-color: white; width: 90%; border-radius: 10px; padding-block: 20px ;margin:0 auto; margin-top: 20px;hight:100px')
+
+
+      let h2El2 = document.createElement('h2');
+      h2El2.textContent = `Name: ${allEmployees[i].fullName} - ID: ${allEmployees[i].employeeId}`;
+      divElInfo.appendChild(h2El2);
+      h2El2.setAttribute('style', 'color:blue')
+
+      let h3El3 = document.createElement('h3');
+      h3El3.textContent = `Department: ${allEmployees[i].department} - Level: ${allEmployees[i].level}`;
+      divElInfo.appendChild(h3El3);
+      h3El3.setAttribute('style', 'color:blue;')
+
+      let h4El4 = document.createElement('h4');
+      h4El4.textContent = `Salary: ${allEmployees[i].salary}`;
+      divElInfo.appendChild(h4El4);
+      h4El4.setAttribute('style', 'color:blue')
+
+    } else {
+
+      const section = document.getElementById("Finance");
+
+      let divElback = document.createElement('div')
+      section.appendChild(divElback);
+      divElback.setAttribute('style', 'display: flex; flex-direction: column; background-color: #0E8388; width: 100%; border-radius: 10px; padding-block: 20px; margin-bottom: 20px')
+
+
+      let imgEl = document.createElement('img')
+      divElback.appendChild(imgEl);
+      imgEl.src = `./assets/${allEmployees[i].fullName}.jpg`;
+      imgEl.setAttribute('style', 'width: 40%; height: auto;  border-radius: 50%;margin-left: 30%;')
+
+      let divElInfo = document.createElement('div');
+      divElback.appendChild(divElInfo);
+      divElInfo.setAttribute('style', 'display: flex; flex-direction: column; align-items: center; background-color: white; width: 90%; border-radius: 10px; padding-block: 20px ;margin:0 auto; margin-top: 20px;hight:100px')
+
+
+      let h2El2 = document.createElement('h2');
+      h2El2.textContent = `Name: ${allEmployees[i].fullName} - ID: ${allEmployees[i].employeeId}`;
+      divElInfo.appendChild(h2El2);
+      h2El2.setAttribute('style', 'color:blue')
+
+      let h3El3 = document.createElement('h3');
+      h3El3.textContent = `Department: ${allEmployees[i].department} - Level: ${allEmployees[i].level}`;
+      divElInfo.appendChild(h3El3);
+      h3El3.setAttribute('style', 'color:blue;')
+
+      let h4El4 = document.createElement('h4');
+      h4El4.textContent = `Salary: ${allEmployees[i].salary}`;
+      divElInfo.appendChild(h4El4);
+      h4El4.setAttribute('style', 'color:blue')
+    }
+  }
+}
+
+
+
+
+
+addEmployeeJson();
+
+rander();
+
+
 
 console.log(allEmployees)
+
